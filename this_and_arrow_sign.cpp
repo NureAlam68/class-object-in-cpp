5b045608d30d0cae91db099371f3a2d693cdcca6
NureAlam68 <nurealam151068@gmail.com>
2025-07-09 20:33:04 +0600

    this and arrow sign

---
+++ this_and_arrow_sign.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Student 
+{
+    public:
+    int roll;
+    int cls;
+    double gpa;
+
+    Student(int roll, int cls, double gpa)
+    {
+        this->roll = roll;
+        this->cls = cls;
+        this->gpa = gpa;
+    }
+};
+
+int main()
+{
+    Student rahim(20, 5, 4.5);
+    Student karim(4, 5, 5);
+    cout << rahim.roll << " " << rahim.cls << " " << rahim.gpa << endl;
+    cout << karim.roll << " " << karim.cls << " " << karim.gpa << endl;
+    return 0;
+}
